c0510fdd683781cd51a99fee78b0100be7ff5054
Studna555 <xstude23@stud.fit.vutbr.cz>
2016-04-25 21:34:50 +0200

    Tkinter
    Tkinter import issue solution

--- calculator.py
+++ calculator.py
@@ -17,7 +17,10 @@
 
 from mathpack import Maths
 
-from tkinter import *
+try: # for Python2
+    from Tkinter import *
+except ImportError: # for Python3
+    from tkinter import *
 
 calc = Maths()
 number=0
